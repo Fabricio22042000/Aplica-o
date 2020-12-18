5fdb450ad9f505c7984990c73f85e656ba6a87ab
Fabricio Moura <fabriciomourafilho@gmail.com>
2020-12-18 11:24:36 -0300

    update

--- src/main/java/com/algaworks/algamoney/api/config/SecurityConfig.java
+++ src/main/java/com/algaworks/algamoney/api/config/SecurityConfig.java
@@ -15,6 +15,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	public AuthenticationManager authenticationManagerBean() throws Exception {
 	   return super.authenticationManagerBean();
 	}
-	
+	 
 	
 }
