25a7d0ff8bf1e720f490e4acb8ccc19483a2a430
Fabricio Moura <fabriciomourafilho@gmail.com>
2020-12-18 22:29:41 -0300

    update

--- src/main/java/com/algaworks/algamoney/api/config/AuthorizationServerConfig.java
+++ src/main/java/com/algaworks/algamoney/api/config/AuthorizationServerConfig.java
@@ -3,6 +3,7 @@ package com.algaworks.algamoney.api.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
@@ -15,6 +16,7 @@ import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
+@Configuration
 @EnableAuthorizationServer
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter{
 	
